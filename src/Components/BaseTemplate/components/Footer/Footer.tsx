a6dd31bbdda6a34d8cf1d132f735552ec6da307c
Augusto Vicente <devguto@gmail.com>
2022-12-04 11:14:57 -0300

    done footers translate

--- src/Components/BaseTemplate/components/Footer/Footer.tsx
+++ src/Components/BaseTemplate/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import { useTranslation } from "react-i18next"
 import { Link } from "react-router-dom"
 
 const Footer = () => {
+    const { t } = useTranslation()
     return (
         <footer className="main-footer">
             <div className="footer-top-wrap">
@@ -11,7 +13,7 @@ const Footer = () => {
                                 <div className="footer-logo mb-25">
                                     <a href="/index"><img src="assets/img/favicon.png" alt="Logo MySticker" /></a>
                                 </div>
-                                <p>My Sticker, a primeira plataforma de criação de álbum de figurinhas.</p>
+                                <p>{t('footer.info')}</p>
                                 <ul className="footer-social">
                                     <li><a href="https://twitter.com/pruu_oficial" target="_blank"><i className="fab fa-twitter" /></a></li>
                                     <li><a href="https://www.instagram.com/pruu_oficial" target="_blank"><i className="fab fa-instagram" /></a></li>
@@ -27,25 +29,25 @@ const Footer = () => {
 
                         <div className="col-lg-2 col-md-3 col-sm-6">
                             <div className="footer-widget">
-                                <h4 className="fw-title">Links úteis</h4>
+                                <h4 className="fw-title">{t('footer.usefull_links')}</h4>
                                 <ul className="fw-links">
-                                    <li><Link to="/album">Álbum</Link></li>
-                                    <li><Link to="/login">Login</Link></li>
-                                    <li><Link to="/register">Cadastrar</Link></li>
-                                    <li><Link to="/marketplace">Loja</Link></li>
-                                    <li><Link to="/rewards">Prêmios</Link></li>
-                                    <li><a href="https://mysticker.gitbook.io/whitepaper-mysticker/" target="_blank">WhitePaper</a></li>
+                                    <li><Link to="/album">{t('footer.album')}</Link></li>
+                                    <li><Link to="/login">{t('footer.login')}</Link></li>
+                                    <li><Link to="/register">{t('footer.register')}</Link></li>
+                                    <li><Link to="/marketplace">{t('footer.store')}</Link></li>
+                                    <li><Link to="/rewards">{t('footer.rewards')}</Link></li>
+                                    <li><a href="https://mysticker.gitbook.io/whitepaper-mysticker/" target="_blank">{t('footer.whitepaper')}</a></li>
                                 </ul>
                             </div>
                         </div>
 
                         <div className="col-lg-2 col-md-3 col-sm-6">
                             <div className="footer-widget">
-                                <h4 className="fw-title">Comunidade</h4>
+                                <h4 className="fw-title">{t('footer.community')}</h4>
                                 <ul className="fw-links">
-                                    <li><Link to="/">Contato</Link></li>
-                                    <li><Link to="/">Ajuda</Link></li>
-                                    <li><Link to="/">Propor Parcerias</Link></li>
+                                    <li><Link to="/">{t('footer.contact')}</Link></li>
+                                    <li><Link to="/">{t('footer.help')}</Link></li>
+                                    <li><Link to="/">{t('footer.be_partner')}</Link></li>
                                 </ul>
                             </div>
                         </div>
